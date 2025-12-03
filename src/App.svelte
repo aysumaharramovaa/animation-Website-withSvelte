eea4d3bc4bf225c076ad4f410cc5efb12fb10840
Aysu Maharramova <125694685+aysumaharramovaa@users.noreply.github.com>
2025-12-04 00:20:34 +0400

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  let name = "Aysu";
+  let count = 0;
+
+  function increment() {
+    count += 1;
+  }
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>Button clicked {count} times.</p>
+  <button on:click={increment}>Click me</button>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    margin-top: 50px;
+    font-family: sans-serif;
+  }
+
+  button {
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+  }
+</style>
